fba3d799897e3314c55f3a18371a0bc1a10b21de
hyun <kimakuma808@gmail.com>
2022-12-13 13:34:59 +0900

    Update: User Schema update

--- backend/src/db/schemas/user.entity.ts
+++ backend/src/db/schemas/user.entity.ts
@@ -1,17 +1,46 @@
-// import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import {Resume} from "./resume.entity";
 
-// export enum roleEnum {
-//   COMPANY = "기업",
-//   USER = "개인",
-//   ADMIN = "관리자",
-// }
-// @Entity()
-// export class Company {
-//   @PrimaryGeneratedColumn()
-//   id: number;
+export enum roleEnum {
+    COMPANY = "기업",
+    USER = "개인",
+    ADMIN = "관리자"
+}
 
-//   @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
-//   created: Date;
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    phoneNumber: string;
+
+    @Column()
+    password: string;
+
+    @Column({ type: "enum", enum: roleEnum })
+    role: roleEnum;
+
+    @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
+    created: Date;
+
+    @Column()
+    active: boolean;
+
+    @OneToMany((type) => Resume, (resume) => resume.resume_id, { nullable: true })
+    //@OneToMany((type) => Topic, (resume) => resume.owner, { nullable: true })
+    resume_id: Resume[];
+}
+
+
+
+
 
 //   @Column()
 //   username: roleEnum;
@@ -19,11 +48,6 @@
 //   @Column({ type: "enum", enum: roleEnum })
 //   role: string;
 
-//   @Column()
-//   email: string;
-
-//   @Column()
-//   phoneNumber: string;
 //   //   @OneToMany((type) => Topic, (topic) => topic.owner, { eager: false })
 //   @OneToMany((type) => Topic, (resume) => resume.owner, { nullable: true })
 //   ownResume: Resume[];
