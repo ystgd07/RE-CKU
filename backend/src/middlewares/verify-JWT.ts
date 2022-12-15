b505d290b8d346667efcef5fa2fbb89cc1fe3907
strombreeding <speaker1403@gmail.com>
2022-12-15 14:39:46 +0900

    FEAT:add 토큰검증미들웨어

--- backend/src/middlewares/verify-JWT.ts
+++ backend/src/middlewares/verify-JWT.ts
@@ -1,40 +1,37 @@
-// import jwt from "jsonwebtoken";
+import { Request, RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+export const tokenValidator: RequestHandler = (req, res, next) => {
+  // request 헤더로부터 authorization bearer 토큰을 받음.
+  if (!req.headers.authorization) {
+    return res.status(403).json({
+      result: "forbidden-approach",
+      msg: "로그인한 유저만 사용할 수 있는 서비스입니다.\n토큰을 제시해 주세요.",
+    });
+  }
+  const userToken = req.headers["authorization"].split(" ")[1];
+  // 해당 token 이 정상적인 token인지 확인
+  try {
+    const secretKey = process.env.JWT_SECRET_KEY || "secret";
+    const jwtDecoded = jwt.verify(userToken, secretKey);
+    req.body = jwtDecoded;
+    next();
+  } catch (error) {
+    // jwt.verify 함수가 에러를 발생시키는 경우는 토큰이 정상적으로 decode 안되었을 경우임.
+    // 403 코드로 JSON 형태로 프론트에 전달함.
+    console.log(error.message);
+    switch (error.message) {
+      case "invalid signature":
+        const notAllow = new Error("404, 우리가 서명한 토큰이 아닙니다.");
+        next(notAllow);
+        break;
+      case "jwt expired":
+        const expired = new Error("403, 만료된 토큰입니다.");
+        next(expired);
+        break;
+      default:
+        next(new Error("400, 토큰 검증 도중 오류가 발생했습니다."));
+    }
+  }
+};
 
-// function loginRequired(req, res, next) {
-//   // request 헤더로부터 authorization bearer 토큰을 받음.
-//   const userToken = req.headers["emailAuth "]?.split(" ")[1];
-//   // 이 토큰은 jwt 토큰 문자열이거나, 혹은 "null" 문자열이거나, undefined임.
-//   // 토큰이 "null" 일 경우, login_required 가 필요한 서비스 사용을 제한함.
-//   if (!userToken || userToken === null || userToken === undefined) {
-//     console.log("서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음");
-//     res.status(403).json({
-//       result: "forbidden-approach",
-//       msg: "로그인한 유저만 사용할 수 있는 서비스입니다.",
-//     });
-
-//     return;
-//   }
-
-//   // 해당 token 이 정상적인 token인지 확인
-//   try {
-//     const secretKey = process.env.JWT_SECRET_KEY || "secret-key";
-//     const jwtDecoded = jwt.verify(userToken, secretKey);
-//     console.log(jwtDecoded);
-//     const { userId, role, sosial } = jwtDecoded;
-
-//     // 라우터에서 req.currentUserId를 통해 유저의 id에 접근 가능하게 됨
-//     req.currentUserId = userId;
-//     req.currentRole = role;
-//     req.currentSosial = sosial;
-//     next();
-//   } catch (error) {
-//     // jwt.verify 함수가 에러를 발생시키는 경우는 토큰이 정상적으로 decode 안되었을 경우임.
-//     // 403 코드로 JSON 형태로 프론트에 전달함.
-//     res.status(403).json({
-//       result: "forbidden-approach",
-//       msg: "정상적인 토큰이 아닙니다.",
-//     });
-
-//     return;
-//   }
-// }
+// };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import { Skill } from "./db/schemas/skill.entity";
 import { Stack } from "./db/schemas/stacks.entity";
 import { errorHandler } from "./middlewares/error-handdler";
 import userRoute from "./routes/user.routes";
+import boardRoute from "./routes/board.routes";
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.use("/users", userRoute);
-app.use("/board", userRoute);
+app.use("/boards", boardRoute);
 
 // 에러 미들웨어
 app.use(errorHandler);
