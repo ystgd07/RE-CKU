72b3bfa0131b95fcb4910ba8092090c3518a19f2
yang <ysotgood@gmail.com>
2022-12-16 02:27:51 +0900

    Auto stash before rebase of "origin/dev"

--- frontend/src/pages/MyPortfolio/index.tsx
+++ frontend/src/pages/MyPortfolio/index.tsx
@@ -1,6 +1,7 @@
-import { Layout } from "./style";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { Layout } from './style';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 // let mocksPortfolio: {
 //   name: string;
 //   address: string;
@@ -42,67 +43,78 @@ import { useEffect, useState } from "react";
 type Mock = { name: string; address: string; date: string; id: any };
 
 const ResumeMain = () => {
-  const [res, setRes] = useState<Mock[]>([]);
-  async function getPortfolio() {
-    try {
-      // const res = await axios.post(
-      //   "https://reactproject-test-78fcd-default-rtdb.firebaseio.com/mock.json",
-      //   { mocksPortfolio }
-      // );
-      const mocks = await axios.get(
-        "https://reactproject-test-78fcd-default-rtdb.firebaseio.com/mock.json"
-      );
-      console.log(mocks.data["-NJJR5a9003Z0Qw6WOzB"]);
-      const mock = mocks.data["-NJJR5a9003Z0Qw6WOzB"].mocksPortfolio;
-      setRes(mock);
-    } catch (e) {
-      console.log(e);
+    const [res, setRes] = useState<Mock[]>([]);
+    const navigate = useNavigate();
+    async function getPortfolio() {
+        try {
+            const token = localStorage.getItem('accessToken');
+            // const res = await axios.post(
+            //   "https://reactproject-test-78fcd-default-rtdb.firebaseio.com/mock.json",
+            //   { mocksPortfolio }
+            // );
+            const mocks = await axios.get('/myportfolio/myportfolio/list', {
+                headers: { authorization: `Bearer ${token}` },
+            });
+            console.log(mocks);
+            // console.log(mocks.data["-NJJR5a9003Z0Qw6WOzB"]);
+            // const mock = mocks.data["-NJJR5a9003Z0Qw6WOzB"].mocksPortfolio;
+            // setRes(mock);
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
-  async function postPortfolio() {
-    try {
-      // const res = await axios.post(
-      //   "https://reactproject-test-78fcd-default-rtdb.firebaseio.com/mock.json",
-      //   { mocksPortfolio }
-      // );
-      const mocks = await axios.post(
-        "https://reactproject-test-78fcd-default-rtdb.firebaseio.com/mock.json"
-      );
-    } catch (e) {
-      console.log(e);
+    async function postPortfolio() {
+        try {
+            const token = localStorage.getItem('accessToken');
+            // const res = await axios.post(
+            //   "https://reactproject-test-78fcd-default-rtdb.firebaseio.com/mock.json",
+            //   { mocksPortfolio }
+            // );
+            const mocks = await axios.post(
+                '/myportfolio/resume/',
+                {},
+                { headers: { authorization: `Bearer ${token}` } },
+            );
+            if (mocks.status === 200) {
+                navigate('/');
+            }
+            console.log(mocks);
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
-  useEffect(() => {
-    getPortfolio();
-  }, []);
 
-  return (
-    <>
-      <h1>My Portfolio</h1>
-      {res.length === 0 ? (
-        <p>작성된 이력서가 없습니다.</p>
-      ) : (
-        <p>작성된 이력서가 {res.length}개 있습니다.</p>
-      )}
-      <Layout>
-        <div>
-          <div>
-            <div>
-              <i>+</i>
-            </div>
-            <p>새 이력서 작성</p>
-          </div>
-        </div>
+    useEffect(() => {
+        getPortfolio();
+    }, []);
 
-        {res.map((e: any) => (
-          <div key={e.id}>
-            <h3>{e.name}</h3>
-            <p>{e.date}</p>
-          </div>
-        ))}
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <h1>My Portfolio</h1>
+            {res.length === 0 ? (
+                <p>작성된 이력서가 없습니다.</p>
+            ) : (
+                <p>작성된 이력서가 {res.length}개 있습니다.</p>
+            )}
+            <Layout>
+                <div onClick={postPortfolio}>
+                    <div>
+                        <div>
+                            <i>+</i>
+                        </div>
+                        <p>새 이력서 작성</p>
+                    </div>
+                </div>
+
+                {res.map((e: any) => (
+                    <div key={e.id}>
+                        <h3>{e.name}</h3>
+                        <p>{e.date}</p>
+                    </div>
+                ))}
+            </Layout>
+        </>
+    );
 };
 
 export default ResumeMain;
