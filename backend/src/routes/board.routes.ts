89683a2822a15f8ec7bd2f8fdc6242039675586a
strombreeding <speaker1403@gmail.com>
2022-12-15 18:18:50 +0900

    FEAT:add API-[보드생성,생성쿼리함수]

--- backend/src/routes/board.routes.ts
+++ backend/src/routes/board.routes.ts
@@ -1,6 +1,8 @@
+import { CreateBoardDto } from "./dto/create-board.dto";
 import express from "express";
 import { postNotice } from "../services/board.service";
 import { tokenValidator } from "../middlewares/verify-JWT";
+import { validateBody } from "../middlewares/dto-validator";
 
 const boardRoute = express();
 boardRoute.post("/jwttest", tokenValidator, (req, res) => {
@@ -9,12 +11,14 @@ boardRoute.post("/jwttest", tokenValidator, (req, res) => {
 });
 
 // 게시글 작성 API
-boardRoute.post("/", tokenValidator, async (req, res, next) => {
+boardRoute.post("/", validateBody(CreateBoardDto), tokenValidator, async (req, res, next) => {
   const { id, role } = req.body.jwtDecoded;
-  const { title, content, hashTags, resumeId } = req.body;
+  const { title, content, hashTags } = req.body;
+  let { resumeId } = req.body;
+  if (resumeId === 0) resumeId = null;
   const data: Record<string, string | boolean | number> = {
-    content,
     fromUserId: id,
+    content,
     title,
     hashTags,
     hasResumeId: resumeId,
@@ -22,6 +26,10 @@ boardRoute.post("/", tokenValidator, async (req, res, next) => {
   console.log("바디의 데이터 : ", data);
   try {
     const result = await postNotice(data);
+    return res.status(201).json({
+      status: 200,
+      data: result,
+    });
   } catch (err) {
     next(err);
   }
