ceadc34f262d87d47599d972b42b6533bfdc80a9
GogoMushroom <yoonzi1003@gmail.com>
2022-12-16 17:15:21 +0900

    벨리데이션 체크중

--- frontend/src/pages/Join/index.tsx
+++ frontend/src/pages/Join/index.tsx
@@ -1,142 +1,225 @@
 import React, { useState } from "react";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import axios from 'axios';
-import * as S  from './style';
+import axios from "axios";
+import * as S from "./style";
 import Logo from "assets/images/iogo.png";
-import { EmailAuth } from './../../../../backend/src/db/schemas/email-auth.entity';
-import e from "express";
-
+import { useForm } from "react-hook-form";
 
 const Join = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [code, setCode] = useState("");
 
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   const [passwordCheck, setPasswordCheck] = useState("");
-   const [name, setName] = useState("");
-   const [phone, setPhone] = useState("");
-   const [code, setCode] = useState("");
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormData>();
 
-   const navigate = useNavigate();
-   const data = {
-      email: email,
-      password: password,
-      username : name,
-      phoneNumber : phone,
-   }
+  interface FormData {
+    id: string;
+    password: string;
+    name: string;
+    phone: string;
+    email: string;
+    code: number;
+    passwordCheck: string;
+  }
 
-   const data2 = {
-      email: email,
-      code : Number(code),
-   }
+  const onValid = async (data: FormData) => {
+    if (data.password !== data.passwordCheck) {
+      setError("passwordCheck", { message: "비밀번호를 다시 확인해주세요." });
+    }
 
+    const jsondata = {
+      email: data.email,
+      password: data.password,
+      username: data.name,
+      phoneNumber: data.phone,
+    };
 
-   const onSubmitHandler = async (e : React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      try {
-         console.log('data', data);
-         const res = await axios.post("/users/individual", data);
-         console.log(res, '성공')
+    try {
+      console.log("data", jsondata);
+      const res = await axios.post("/users/individual", jsondata);
+      console.log(res, "성공");
 
-         alert("회원가입이 완료되었습니다.");
-         navigate('/login');
+      alert("회원가입이 완료되었습니다.");
+      navigate("/login");
+    } catch (err: any) {
+      console.error(err.stack);
+    }
+  };
 
-      } catch (err : any)  {
-         console.error(err.stack);
-         
-      }
-   }
+  const sendEmail = async (data: FormData) => {
+    const jsondata = {
+      email: data.email,
+    };
+    console.log("jsondata", jsondata);
 
-      const sendEmail = async (e : any ) => {
-         e.preventDefault();
-         console.log('email', email);
-         
-         try {
-            const res = await axios.post("/users/email",{ email } );
-            console.log(res, '성공')
+    try {
+      const res = await axios.post("/users/email", jsondata);
+      console.log(res, "성공");
+    } catch (err: any) {
+      console.error(err.stack);
+    }
+  };
 
-         } catch (err : any)  {
-            console.error(err.stack);
-            
+  const emailAuth = async (data: FormData) => {
+    const jsondata = {
+      email: data.email,
+      code: Number(data.code),
+    };
 
-         }
-   };
+    console.log("data", jsondata);
 
-   const emailAuth = async (e : any ) => {
-      e.preventDefault();
-      console.log('data', data2);
-      
-      try {
-         const res = await axios.post("/users/email/auth", data2 );
-         console.log(res, '성공')
+    try {
+      const res = await axios.post("/users/email/auth", jsondata);
+      console.log(res, "성공");
 
-         const reqConfirm = res.data.msg
+      const reqConfirm = res.data.msg;
 
-         sessionStorage.setItem("reqConfirm", reqConfirm);
-         sessionStorage.setItem("email", email);
+      sessionStorage.setItem("reqConfirm", reqConfirm);
+      sessionStorage.setItem("email", data.email);
+    } catch (err: any) {
+      console.error(err.stack);
+    }
+  };
 
-      } catch (err : any)  {
-         console.error(err.stack);
-         
+  const errStyle = () => {
+    if (password !== passwordCheck) {
+      return { border: "3px solid red" };
+    }
+  };
 
-         }
-   };
+  const errjoin = () => {
+    if (!(email && password && passwordCheck && name && phone && code)) {
+      return { display: "none" };
+    }
+    if (password !== passwordCheck) {
+      return { display: "none" };
+    }
+    if (!(email === sessionStorage.getItem("email"))) {
+      return { display: "none" };
+    }
+    if (!sessionStorage.getItem("reqConfirm")) {
+      return { display: "none" };
+    }
+  };
 
-
-
-   const errStyle = () => {
-      if(password !== passwordCheck){
-         return {border : "3px solid red"}
-
-      }
-   }
-
-   const errjoin = () => {
-         
-         if(!(email && password && passwordCheck && name &&  phone && code) ){
-            return { display: "none" } 
-         }
-         if(( password !== passwordCheck)){
-            return { display: "none" }
-         }
-         if(!(email === sessionStorage.getItem('email'))){
-            return { display: "none" }
-         }
-         if(!(sessionStorage.getItem('reqConfirm'))){
-            return { display: "none" }
-         }
-   }
-
-   return (
-      <S.Div>
-         <S.MobileDiv >
-            <p ><S.Image src={Logo} alt="로고"/></p>
-            <form onSubmit={onSubmitHandler}>
-               <div style={{ display : "flex", flexDirection: "column", width: "400px" ,minWidth:"320px" , textAlign: "center",paddingBottom: "30px"  }}>
-                  <h1>회원가입</h1>
-                  <p>이메일</p>
-                  <input type="email" value={email} placeholder='이메일 입력' onChange={e => {setEmail(e.currentTarget.value)}}/>
-                     <button onClick={sendEmail} >인증번호 보내기</button>
-                  <div style={{ display: "flex" ,textAlign: "left" }}>
-                     <input placeholder='인증번호를 입력해주세요' value={code} onChange={e => {setCode(e.currentTarget.value)}}></input>
-                  <button onClick={emailAuth}>인증하기</button>
-                  </div>
-                  <p>비밀번호</p>
-                  <input type="password" value={password} placeholder='비밀번호 입력' onChange={e => {setPassword(e.currentTarget.value)}}/>
-                  <p>비밀번호 확인</p>
-                  <input type="password" style={errStyle()}  value={passwordCheck} placeholder='비밀번호 입력' onChange={e => {setPasswordCheck(e.currentTarget.value)}} />
-                  <p>이름</p>
-                  <input type="string"  value={name} placeholder='이름 입력' onChange={e => {setName(e.currentTarget.value)}}/> 
-                  <p>휴대폰</p>
-                  <input type="string" value={phone} placeholder='- 빼고 입력해주세요' onChange={e => {setPhone(e.currentTarget.value)}}/> 
-                  <button  formAction='' style={errjoin()} >회원가입</button> 
-                  <Link to="/login" style={{ textDecoration: 'none' , color:"black", fontSize: "10px", fontWeight:"bold" }}>로그인  </Link> 
-               </div>
-            </form>
-         </S.MobileDiv>
-      </S.Div>
-      )
-   };
-
+  return (
+    <S.Div>
+      <S.MobileDiv>
+        <p>
+          <div>
+            <Link to="/">
+              {" "}
+              <S.Image src={Logo} alt="로고" />
+            </Link>
+          </div>
+        </p>
+        <form onSubmit={handleSubmit(onValid)} autoComplete="off">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "400px",
+              minWidth: "320px",
+              textAlign: "center",
+              paddingBottom: "30px",
+            }}
+          >
+            <h1>회원가입</h1>
+            <p>이메일</p>
+            <input
+              {...register("email", {
+                required: "이메일을 입력해주세요",
+                pattern: {
+                  value: /^[A-Za-z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                  message: "이메일 형식만 가능합니다.",
+                },
+              })}
+              placeholder="이메일 입력"
+              autoComplete="off"
+            />
+            <label>{errors?.email?.message}</label>
+            {/* <label>아무말</label> */}
+            <button
+              style={{ marginTop: "10px" }}
+              onClick={handleSubmit(sendEmail)}
+            >
+              인증번호 보내기
+            </button>
+            <div style={{ display: "flex", textAlign: "left" }}>
+              <input
+                placeholder="인증번호를 입력해주세요"
+                {...register("code")}
+              ></input>
+              <button onClick={handleSubmit(emailAuth)}>인증하기</button>
+            </div>
+            <p>비밀번호</p>
+            <input
+              {...register("password", {
+                required: "비밀번호를 입력해주세요",
+                minLength: {
+                  value: 5,
+                  message: "비밀번호는 최소 5자리 이상 입력해주세요",
+                },
+              })}
+              type="password"
+              placeholder="비밀번호 입력"
+              autoComplete="new-password"
+            />
+            <label>{errors?.password?.message}</label>
+            <p>비밀번호 확인</p>
+            <input
+              type="password"
+              //   style={errStyle()}
+              placeholder="비밀번호 확인"
+              {...register("passwordCheck", {
+                required: "비밀번호를 입력해주세요 ",
+              })}
+            />
+            <label>{errors?.passwordCheck?.message}</label>
+            <p>이름</p>
+            <input
+              {...register("name", {
+                required: "비밀번호를 입력해주세요 ",
+              })}
+              placeholder="이름 입력"
+            />
+            <p>휴대폰</p>
+            <input
+              {...register("phone", {
+                required: "비밀번호를 입력해주세요 ",
+              })}
+              placeholder="- 빼고 입력해주세요"
+            />
+            <button formAction="" style={errjoin()}>
+              회원가입
+            </button>
+            <Link
+              to="/login"
+              style={{
+                textDecoration: "none",
+                color: "black",
+                fontSize: "10px",
+                fontWeight: "bold",
+                marginTop: "10px",
+              }}
+            >
+              로그인{" "}
+            </Link>
+          </div>
+        </form>
+      </S.MobileDiv>
+    </S.Div>
+  );
+};
 
 export default Join;
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,66 +1,124 @@
 import React, { useState } from "react";
-import * as S  from './style';
+import * as S from "./style";
 import Logo from "assets/images/iogo.png";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import axios from 'axios';
-import e from "express";
+import axios from "axios";
+import { useForm } from "react-hook-form";
 
-
-interface hey  {
-   email : string, 
-   password : string
+interface hey {
+  email: string;
+  password: string;
 }
 
-   const Login = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-      const [email, setEmail] = useState("");
-      const [password, setPassword] = useState("");
-      const navigate = useNavigate();
-      const data = {
-         email: email,
-         password: password,
-      }
-      const onSubmitHandler = async (e : React.FormEvent<HTMLFormElement>) => {
-         e.preventDefault();
-         console.log('Email', email);
-         console.log('Password', password);
-         try {
-            console.log('data', data);
-            const res = await axios.post("/users", data);
-            console.log(res, '성공')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormData>();
 
-               const accessToken = res.data.accessToken;
-               const refreshToken = res.data.refreshToken;
-               localStorage.setItem("accessToken", accessToken);
-               localStorage.setItem("refreshToken", refreshToken);
-               localStorage.setItem("email", email);
-               navigate('/');
+  interface FormData {
+    password: string;
+    email: string;
+  }
 
-         } catch (err : any)  {
-            console.error(err.stack);
-            alert("아이디 혹은 비밀번호가 틀렸습니다");
-         }
-      };
-      
-   return (
-      <S.Div>
-         <S.MobileDiv >
-            <form  onSubmit={onSubmitHandler}>
-            <p ><S.Image src={Logo} alt="로고"/></p>
-            <div style={{width: "400px" ,minWidth:"320px" , textAlign: "center" , paddingBottom: "30px" }}>
+  const onSubmitHandler = async (data: FormData) => {
+    const jsondata = {
+      email: data.email,
+      password: data.password,
+    };
+    try {
+      console.log("jsondata", jsondata);
+      const res = await axios.post("/users", jsondata);
+      console.log(res, "성공");
+
+      const accessToken = res.data.accessToken;
+      const refreshToken = res.data.refreshToken;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem("email", data.email);
+      navigate("/");
+    } catch (err: any) {
+      console.error(err.stack);
+      alert("아이디 혹은 비밀번호가 틀렸습니다");
+    }
+  };
+
+  return (
+    <S.Div>
+      <S.MobileDiv>
+        <form onSubmit={handleSubmit(onSubmitHandler)} autoComplete="off">
+          <p>
+            <Link to="/">
+              {" "}
+              <S.Image src={Logo} alt="로고" />
+            </Link>
+          </p>
+          <div
+            style={{
+              width: "400px",
+              minWidth: "320px",
+              textAlign: "center",
+              paddingBottom: "30px",
+            }}
+          >
             <h1>로그인</h1>
-            <input type="email" value={email}  onChange={e => {setEmail(e.currentTarget.value)}} placeholder='이메일 입력' /> 
-            <input type="password" value={password} onChange={e => {setPassword(e.currentTarget.value)}}  placeholder='비밀번호 입력'/>
-            <button type="submit" >로그인</button> 
-            <Link to="/Join" style={{ textDecoration: 'none' , color:"black", fontSize: "10px", fontWeight:"bold",margin:"10px" }}>회원가입  </Link> 
-            <Link to="/FindPw" style={{ textDecoration: 'none' , color:"black", fontSize: "10px",margin:"10px" }}>비밀번호를 잊으셨나요?</Link>
-            </div>
-            </form>
-         </S.MobileDiv>
-      </S.Div>
-      )
-   };
-
+            <input
+              type="email"
+              {...register("email", {
+                required: "이메일을 입력해주세요",
+                pattern: {
+                  value: /^[A-Za-z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                  message: "이메일 형식만 가능합니다.",
+                },
+              })}
+              placeholder="이메일 입력"
+              autoComplete="off"
+            />
+            <label>{errors?.email?.message}</label>
+            <input
+              {...register("password", {
+                required: "비밀번호를 입력해주세요",
+              })}
+              type="password"
+              placeholder="비밀번호 입력"
+              autoComplete="new-password"
+            />
+            <button type="submit">로그인</button>
+            <Link
+              to="/Join"
+              style={{
+                textDecoration: "none",
+                color: "black",
+                fontSize: "10px",
+                fontWeight: "bold",
+                margin: "10px",
+              }}
+            >
+              회원가입{" "}
+            </Link>
+            <Link
+              to="/FindPw"
+              style={{
+                textDecoration: "none",
+                color: "black",
+                fontSize: "10px",
+                margin: "10px",
+              }}
+            >
+              비밀번호를 잊으셨나요?
+            </Link>
+          </div>
+        </form>
+      </S.MobileDiv>
+    </S.Div>
+  );
+};
 
 export default Login;
