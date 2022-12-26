2ea758853479acafc0d28f85967c33f7e1fa26cb
GogoMushroom <yoonzi1003@gmail.com>
2022-12-26 16:17:09 +0900

    어드민 라우팅중

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Main from 'pages/Main';
 import Login from 'pages/Login';
 import ResumeEdit from 'pages/ResumeEdit';
-import Post from 'pages/Post';
+// import Post from 'pages/Post';
 import PostCreate from 'pages/PostCreate';
 import Join from 'pages/Join';
 import FindPw from 'pages/FindPw';
@@ -16,35 +16,44 @@ import AdminUser from 'components/Admin/AdminUser';
 import AdminContent from 'components/Admin/AdminContent';
 const Router = () => {
     const isLogined = localStorage.getItem('accessToken') ? true : false;
+    const isAdmin = localStorage.getItem('isAdmin') ? true : false;
     // <Route> => <BasicRouter>, <AdminRouter>, <AuthRouter>
     return (
         <BrowserRouter>
             <Routes>
                 {isLogined ? (
                     <>
-                        <Route path="/" element={<Main />} />
-                        <Route path="/resume/list" element={<ResumeList />} />
-                        <Route path="/resume/:id" element={<Resume />} />
-                        <Route path="/resume/:id/edit" element={<ResumeEdit />} />
-                        <Route path="/post/:postId" element={<Post />} />
-                        <Route path="/post/:postId/edit" element={<PostCreate />} />
-                        <Route path="/post/create" element={<PostCreate />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/login" element={<Navigate replace to="/" />} />
-                        <Route path="/find-pw" element={<Navigate replace to="/" />} />
-                        <Route path="/join" element={<Navigate replace to="/" />} />
-                        <Route path="/admin" element={<Admin />}>
-                            <Route path="user" element={<AdminUser />} />
-                            <Route path="content" element={<AdminContent />} />
-                        </Route>
-                        <Route path="/loading" element={<Loading />} />
-                        <Route path="/*" element={<Err404 />} />
+                        {isAdmin ? (
+                            <>
+                                <Route path="/admin" element={<Admin />}>
+                                    <Route path="user" element={<AdminUser />} />
+                                    <Route path="content" element={<AdminContent />} />
+                                </Route>
+                            </>
+                        ) : (
+                            <>
+                                <Route path="/" element={<Main />} />
+                                <Route path="/resume/list" element={<ResumeList />} />
+                                <Route path="/resume/:id" element={<Resume />} />
+                                <Route path="/resume/:id/edit" element={<ResumeEdit />} />
+                                {/* <Route path="/post/:postId" element={<Post />} /> */}
+                                {/* <Route path="/post/:postId/edit" element={<PostCreate />} /> */}
+                                {/* <Route path="/post/create" element={<PostCreate />} /> */}
+                                <Route path="/profile" element={<Profile />} />
+                                <Route path="/login" element={<Navigate replace to="/" />} />
+                                <Route path="/find-pw" element={<Navigate replace to="/" />} />
+                                <Route path="/join" element={<Navigate replace to="/" />} />
+
+                                <Route path="/loading" element={<Loading />} />
+                                <Route path="/*" element={<Err404 />} />
+                            </>
+                        )}
                     </>
                 ) : (
                     <>
                         <Route path="/" element={<Main />} />
                         <Route path="/resume/:id" element={<Navigate replace to="/" />} />
-                        <Route path="/post/create" element={<PostCreate />} />
+                        {/* <Route path="/post/create" element={<PostCreate />} /> */}
                         <Route path="/post/:postId" element={<Navigate replace to="/" />} />
                         <Route path="/myportfolio" element={<Navigate replace to="/" />} />
                         <Route path="/profile" element={<Navigate replace to="/" />} />
--- frontend/src/pages/PostCreate/index.tsx
+++ frontend/src/pages/PostCreate/index.tsx
@@ -79,208 +79,209 @@ interface IBoardInfo {
 function PostCreate() {
     const navigate = useNavigate();
 
-    // undefined: 게시물 생성, postId: 해당 게시물 수정
-    const { postId } = useParams<{ postId: string }>();
-    const { state } = useLocation() as RouteState;
+    // // undefined: 게시물 생성, postId: 해당 게시물 수정
+    // const { postId } = useParams<{ postId: string }>();
+    // const { state } = useLocation() as RouteState;
 
-    // 마크다운 에디터 객체
-    const editorRef = useRef<Editor>(null);
-    // 이력서 첨부 여부 상태
-    const [isResume, setIsResume] = useState<boolean>(false);
-    // 폼 입력 데이터
-    const [form, setForm] = useState({
-        title: '',
-        hashTags: '',
-    });
-    const { title, hashTags } = form;
+    // // 마크다운 에디터 객체
+    // const editorRef = useRef<Editor>(null);
+    // // 이력서 첨부 여부 상태
+    // const [isResume, setIsResume] = useState<boolean>(false);
+    // // 폼 입력 데이터
+    // const [form, setForm] = useState({
+    //     title: '',
+    //     hashTags: '',
+    // });
+    // const { title, hashTags } = form;
 
-    // 폼 제출 시 에러 발생한 항목에 에러 메세지 출력을 위한 상태값
-    const [error, setError] = useState({
-        resume: false,
-        title: false,
-        content: false,
-    });
+    // // 폼 제출 시 에러 발생한 항목에 에러 메세지 출력을 위한 상태값
+    // const [error, setError] = useState({
+    //     resume: false,
+    //     title: false,
+    //     content: false,
+    // });
 
-//     const onToggleButton = () => {
-//         setIsResume(prev => !prev);
-//         console.log('Resume :', isResume);
-//     };
+    // //     const onToggleButton = () => {
+    // //         setIsResume(prev => !prev);
+    // //         console.log('Resume :', isResume);
+    // //     };
 
-//     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//         e.preventDefault();
-//         const { name, value } = e.target;
-//         setForm({
-//             ...form,
-//             [name]: value,
-//         });
-//     };
+    // //     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // //         e.preventDefault();
+    // //         const { name, value } = e.target;
+    // //         setForm({
+    // //             ...form,
+    // //             [name]: value,
+    // //         });
+    // //     };
 
-//     const handleSubmit = async () => {
-//         // 게시물 작성 폼 유효성 검사
-//         // 이력서 on 인데 이력서를 선택하지 않았을 경우
-//         if (isResume === true) {
-//             // 이력서 선택 여부 추가 필요
-//             setError({
-//                 ...error,
-//                 resume: true,
-//             });
-//             console.log('이력서를 선택해주세요.');
-//             return;
-//         }
-//         // 제목 입력 여부 판별
-//         if (form.title === '') {
-//             setError({
-//                 ...error,
-//                 title: true,
-//             });
-//             console.log('제목을 입력해주세요.');
-//             return;
-//         }
-//         // 게시물 내용 입력 여부 판별
-//         const content = editorRef.current?.getInstance().getMarkdown();
-//         if (content === '') {
-//             setError({
-//                 ...error,
-//                 content: true,
-//             });
-//             // 에디터 포커스
-//             editorRef.current?.getInstance().focus();
-//             console.log('게시물 내용을 입력해주세요.');
-//             return;
-//         }
+    // //     const handleSubmit = async () => {
+    // //         // 게시물 작성 폼 유효성 검사
+    // //         // 이력서 on 인데 이력서를 선택하지 않았을 경우
+    // //         if (isResume === true) {
+    // //             // 이력서 선택 여부 추가 필요
+    // //             setError({
+    // //                 ...error,
+    // //                 resume: true,
+    // //             });
+    // //             console.log('이력서를 선택해주세요.');
+    // //             return;
+    // //         }
+    // //         // 제목 입력 여부 판별
+    // //         if (form.title === '') {
+    // //             setError({
+    // //                 ...error,
+    // //                 title: true,
+    // //             });
+    // //             console.log('제목을 입력해주세요.');
+    // //             return;
+    // //         }
+    // //         // 게시물 내용 입력 여부 판별
+    // //         const content = editorRef.current?.getInstance().getMarkdown();
+    // //         if (content === '') {
+    // //             setError({
+    // //                 ...error,
+    // //                 content: true,
+    // //             });
+    // //             // 에디터 포커스
+    // //             editorRef.current?.getInstance().focus();
+    // //             console.log('게시물 내용을 입력해주세요.');
+    // //             return;
+    // //         }
 
-        const resumeId = isResume ? 0 : 0;
-        const data = {
-            ...form,
-            content,
-            resumeId,
-        };
+    // const resumeId = isResume ? 0 : 0;
+    // const data = {
+    //     ...form,
+    //     // content,
+    //     resumeId,
+    // };
 
-        try {
-            if (!postId) {
-                const res = await API.post('/board', data);
-                navigate(`/post/${String(res.data)}`);
-            } else {
-                await API.patch(`/board/${postId}`, '', data);
-                navigate(`/post/${postId}`);
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
+    // try {
+    //     if (!postId) {
+    //         const res = await API.post('/board', data);
+    //         navigate(`/post/${String(res.data)}`);
+    //     } else {
+    //         await API.patch(`/board/${postId}`, '', data);
+    //         navigate(`/post/${postId}`);
+    //     }
+    // } catch (err) {
+    //     console.log(err);
+    // }
+    // // };
 
-    const updateEditorContent = (content: string) => {
-        editorRef.current?.getInstance().setMarkdown(content);
-    };
+    // const updateEditorContent = (content: string) => {
+    //     editorRef.current?.getInstance().setMarkdown(content);
+    // };
 
-    useEffect(() => {
-        if (editorRef.current) {
-            editorRef.current.getInstance().removeHook('addImageBlobHook');
-            type HookCallback = (url: string, text?: string) => void;
-            editorRef.current
-                .getInstance()
-                .addHook('addImageBlobHook', async (blob: File, callback: HookCallback) => {
-                    // blob 자체가 file 임,
-                    const formData = new FormData();
-                    // 아래와 같이 저장하면 formData {image:blob} 형태가 됨
-                    formData.append('image', blob);
-                    // 서버에 이미지 저장 및 저장된 이미지 url 응답 받기
-                    const url = await API.post('/file/url', formData);
-                    // 에디터에 url과 파일 이름을 이용한 마크다운 이미지 문법 작성 콜백 함수
-                    callback(process.env.REACT_APP_SERVER_URL + url.imageUrl, blob.name);
-                    return false;
-                });
-        }
+    // useEffect(() => {
+    //     if (editorRef.current) {
+    //         editorRef.current.getInstance().removeHook('addImageBlobHook');
+    //         type HookCallback = (url: string, text?: string) => void;
+    //         editorRef.current
+    //             .getInstance()
+    //             .addHook('addImageBlobHook', async (blob: File, callback: HookCallback) => {
+    //                 // blob 자체가 file 임,
+    //                 const formData = new FormData();
+    //                 // 아래와 같이 저장하면 formData {image:blob} 형태가 됨
+    //                 formData.append('image', blob);
+    //                 // 서버에 이미지 저장 및 저장된 이미지 url 응답 받기
+    //                 const url = await API.post('/file/url', formData);
+    //                 // 에디터에 url과 파일 이름을 이용한 마크다운 이미지 문법 작성 콜백 함수
+    //                 callback(process.env.REACT_APP_SERVER_URL + url.imageUrl, blob.name);
+    //                 return false;
+    //             });
+    //     }
 
-        // 게시물 수정일 경우 기존 내용 채우기
-        if (postId) {
-            updateOriginBoardData();
-        }
+    //     // 게시물 수정일 경우 기존 내용 채우기
+    //     if (postId) {
+    //         updateOriginBoardData();
+    //     }
 
-        return () => {};
-    }, []);
+    //     return () => {};
+    // }, []);
 
-    const updateOriginBoardData = async () => {
-        // 게시물 페이지 수정 버튼으로 접근한 경우
-        if (state) {
-            setForm({
-                title: state.title,
-                hashTags: state.hashTags,
-            });
-            updateEditorContent(state.content);
-            return;
-        }
+    // const updateOriginBoardData = async () => {
+    //     // 게시물 페이지 수정 버튼으로 접근한 경우
+    //     if (state) {
+    //         setForm({
+    //             title: state.title,
+    //             hashTags: state.hashTags,
+    //         });
+    //         updateEditorContent(state.content);
+    //         return;
+    //     }
 
-        // url을 임의로 입력해서 접근한 경우
-        const res = await API.get(
-            `/board/${postId}`,
-            `?lifeIsGood=${localStorage.getItem('userId')}`,
-        );
+    //     // url을 임의로 입력해서 접근한 경우
+    //     const res = await API.get(
+    //         `/board/${postId}`,
+    //         `?lifeIsGood=${localStorage.getItem('userId')}`,
+    //     );
 
-        const data = res.boardInfo;
+    //     const data = res.boardInfo;
 
-        const result = {
-            title: data.title,
-            content: data.content,
-            hashTags: data.hashTags,
-        };
+    //     const result = {
+    //         title: data.title,
+    //         content: data.content,
+    //         hashTags: data.hashTags,
+    //     };
 
-        setForm({
-            title: result?.title,
-            hashTags: result.hashTags,
-        });
-        updateEditorContent(result.content);
-    };
+    //     setForm({
+    //         title: result?.title,
+    //         hashTags: result.hashTags,
+    //     });
+    //     updateEditorContent(result.content);
+    // };
 
     return (
-        <Container>
-            <Wrapper>
-                <WrapperHeader>
-                    <WrapperTitle>이력서 첨부</WrapperTitle>
-                    <ToggleDiv>
-                        <Switch onClick={onToggleButton} />
-                    </ToggleDiv>
-                </WrapperHeader>
+        <></>
+        // <Container>
+        //     <Wrapper>
+        //         <WrapperHeader>
+        //             <WrapperTitle>이력서 첨부</WrapperTitle>
+        //             <ToggleDiv>
+        //                 <Switch onClick={onToggleButton} />
+        //             </ToggleDiv>
+        //         </WrapperHeader>
+        //         // {isResume && <ResumeSelectUI />}
+        //         //{' '}
+        //     </Wrapper>
 
-//                 {isResume && <ResumeSelectUI />}
-//             </Wrapper>
-
-            <Wrapper>
-                <Title level={4}>제목</Title>
-                <Input
-                    size="large"
-                    placeholder="제목을 입력해주세요."
-                    name="title"
-                    value={title}
-                    onChange={onChange}
-                />
-            </Wrapper>
-            <Wrapper>
-                <Title level={4}>내용</Title>
-                <Editor
-                    placeholder="이 입력폼은 마크다운 문법을 지원합니다."
-                    previewStyle="vertical"
-                    height="600px"
-                    initialEditType="markdown"
-                    useCommandShortcut={true}
-                    language="ko-KR"
-                    plugins={[colorSyntax]}
-                    ref={editorRef}
-                />
-            </Wrapper>
-            <Wrapper>
-                <Title level={4}>태그</Title>
-                <Input size="large" name="hashTags" value={hashTags} onChange={onChange} />
-            </Wrapper>
-            <ButtonDiv>
-                <Button type="default" size="large">
-                    취소
-                </Button>
-                <Button type="primary" size="large" onClick={handleSubmit}>
-                    {postId ? '수정' : '등록'}
-                </Button>
-            </ButtonDiv>
-        </Container>
+        //     <Wrapper>
+        //         <Title level={4}>제목</Title>
+        //         <Input
+        //             size="large"
+        //             placeholder="제목을 입력해주세요."
+        //             name="title"
+        //             value={title}
+        //             onChange={onChange}
+        //         />
+        //     </Wrapper>
+        //     <Wrapper>
+        //         <Title level={4}>내용</Title>
+        //         <Editor
+        //             placeholder="이 입력폼은 마크다운 문법을 지원합니다."
+        //             previewStyle="vertical"
+        //             height="600px"
+        //             initialEditType="markdown"
+        //             useCommandShortcut={true}
+        //             language="ko-KR"
+        //             plugins={[colorSyntax]}
+        //             ref={editorRef}
+        //         />
+        //     </Wrapper>
+        //     <Wrapper>
+        //         <Title level={4}>태그</Title>
+        //         <Input size="large" name="hashTags" value={hashTags} onChange={onChange} />
+        //     </Wrapper>
+        //     <ButtonDiv>
+        //         <Button type="default" size="large">
+        //             취소
+        //         </Button>
+        //         <Button type="primary" size="large" onClick={handleSubmit}>
+        //             {postId ? '수정' : '등록'}
+        //         </Button>
+        //     </ButtonDiv>
+        // </Container>
     );
 }
 
