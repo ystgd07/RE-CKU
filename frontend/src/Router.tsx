9744527a5ccb56e2d866e780fcbd6941424e9934
김진영 <bayajeu@gmail.com>
2022-12-28 15:22:52 +0900

    FIX: 라우터 수정

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Main from 'pages/Main';
 import Login from 'pages/Login';
 import ResumeEdit from 'pages/ResumeEdit';
-// import Post from 'pages/Post';
-// import PostCreate from 'pages/PostCreate';
+import Post from 'pages/Post';
+import PostCreate from 'pages/PostCreate';
 import Join from 'pages/Join';
 import FindPw from 'pages/FindPw';
 import ResumeList from 'pages/ResumeList';
@@ -27,70 +27,40 @@ const Router = () => {
             <Routes>
                 {isLogined ? (
                     <>
-                        {isAdmin ? (
+                        {isAdmin && (
                             <>
+                                {/* Admin 전용 라우터 */}
                                 <Route path="/admin" element={<Admin />}>
                                     <Route path="user" element={<AdminUser />} />
                                     <Route path="content" element={<AdminContent />} />
                                 </Route>
-                                <Route path="/" element={<Main />} />
-                                <Route path="/comunity" element={<Comunity />} />
-                                <Route path="/resume/list" element={<ResumeList />} />
-                                <Route path="/resume/:id" element={<Resume />} />
-                                <Route path="/resume/:id/edit" element={<ResumeEdit />} />
-                                {/* <Route path="/post/:postId" element={<Post />} /> */}
-                                {/* <Route path="/post/:postId/edit" element={<PostCreate />} /> */}
-                                {/* <Route path="/post/create" element={<PostCreate />} /> */}
-                                <Route path="/profile" element={<Profile />} />
-                                <Route path="/login" element={<Navigate replace to="/" />} />
-                                <Route path="/find-pw" element={<Navigate replace to="/" />} />
-                                <Route path="/join" element={<Navigate replace to="/" />} />
-
-                                <Route path="/loading" element={<Loading />} />
-                                <Route path="/match" element={<Match />} />
-                                <Route path="/matched" element={<Matched />} />
-                                <Route path="/*" element={<Err404 />} />
-                            </>
-                        ) : (
-                            <>
-                                <Route path="/" element={<Main />} />
-                                <Route path="/comunity" element={<Comunity />} />
-                                <Route path="/resume/list" element={<ResumeList />} />
-                                <Route path="/resume/:id" element={<Resume />} />
-                                <Route path="/resume/:id/edit" element={<ResumeEdit />} />
-                                {/* <Route path="/post/:postId" element={<Post />} /> */}
-                                {/* <Route path="/post/:postId/edit" element={<PostCreate />} /> */}
-                                {/* <Route path="/post/create" element={<PostCreate />} /> */}
-                                <Route path="/profile" element={<Profile />} />
-                                <Route path="/login" element={<Navigate replace to="/" />} />
-                                <Route path="/find-pw" element={<Navigate replace to="/" />} />
-                                <Route path="/join" element={<Navigate replace to="/" />} />
-
-                                <Route path="/loading" element={<Loading />} />
-                                <Route path="/*" element={<Err404 />} />
                             </>
                         )}
+                        <>
+                            <Route path="/" element={<Main />} />
+                            <Route path="/comunity" element={<Comunity />} />
+                            <Route path="/resume/list" element={<ResumeList />} />
+                            <Route path="/resume/:id" element={<Resume />} />
+                            <Route path="/resume/:id/edit" element={<ResumeEdit />} />
+                            <Route path="/post/:postId" element={<Post />} />
+                            <Route path="/post/:postId/edit" element={<PostCreate />} />
+                            <Route path="/post/create" element={<PostCreate />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/loading" element={<Loading />} />
+
+                            <Route path="/login" element={<Navigate replace to="/" />} />
+                            <Route path="/join" element={<Navigate replace to="/" />} />
+                            <Route path="/find-pw" element={<Navigate replace to="/" />} />
+
+                            <Route path="/*" element={<Err404 />} />
+                        </>
                     </>
                 ) : (
                     <>
-                        <Route path="/" element={<Main />} />
-
-                        <Route path="/comunity" element={<Comunity />} />
-                        <Route path="/resume/:id" element={<Navigate replace to="/" />} />
-                        {/* <Route path="/post/create" element={<PostCreate />} /> */}
-                        {/* <Route path="/post/:postId" element={<Navigate replace to="/" />} /> */}
-                        <Route path="/resume/list" element={<Navigate replace to="/" />} />
-                        <Route path="/profile" element={<Navigate replace to="/" />} />
                         <Route path="/login" element={<Login />} />
-                        <Route path="/find-pw" element={<FindPw />} />
                         <Route path="/join" element={<Join />} />
-                        <Route path="/*" element={<Err404 />} />
-                        <Route path="/admin" element={<Admin />}>
-                            <Route path="user" element={<AdminUser />} />
-                            <Route path="content" element={<AdminContent />} />
-                        </Route>
-                        <Route path="/loading" element={<Loading />} />
-                        <Route path="/*" element={<Err404 />} />
+                        <Route path="/find-pw" element={<FindPw />} />
+                        <Route path="/*" element={<Login />} />
                     </>
                 )}
             </Routes>
