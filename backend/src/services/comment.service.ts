9acdcea78004f33684026776daad2c737552882d
김진영 <bayajeu@gmail.com>
2022-12-29 17:43:27 +0900

    FIX: API 에러 수정

--- backend/src/services/comment.service.ts
+++ backend/src/services/comment.service.ts
@@ -1,92 +1,96 @@
-import * as commentRepo from "../db/comment.repo";
-import * as userRepo from "../db/user.repo";
+import * as commentRepo from '../db/comment.repo';
+import * as userRepo from '../db/user.repo';
 
 // 확인
 export const commentLikes = async (userId: number, commentId: number, likesStatus: boolean): Promise<boolean> => {
-  const data = {
-    userId,
-    commentId,
-  };
-  try {
-    // 댓글에 좋아요 여부
-    const alreadyLikes = await commentRepo.alreadyLikesComment(commentId, userId);
-    console.log("alreadyLikeszzz : ", alreadyLikes);
+   const data = {
+      userId,
+      commentId,
+   };
+   try {
+      // 댓글에 좋아요 여부
+      const alreadyLikes = await commentRepo.alreadyLikesComment(commentId, userId);
+      console.log('alreadyLikeszzz : ', alreadyLikes);
 
-    // 좋아요 하지 않은 댓글이라면 좋아요 및 포인트 습득
-    if (!likesStatus && !alreadyLikes) {
-      await commentRepo.likeCommentFromUser(data);
+      // 좋아요 하지 않은 댓글이라면 좋아요 및 포인트 습득
+      if (!likesStatus && !alreadyLikes) {
+         await commentRepo.likeCommentFromUser(data);
 
-      // 아래 변수명에 포인트 적립여부가 담겨있음
-      const alreadySavePoint = await commentRepo.findSavedPointByComment(userId, commentId);
-      console.log("포인트받음?", alreadySavePoint);
-      // 첫번째 좋아요라 포인트적립이 되지 않았을 경우 적립
-      if (!alreadySavePoint) {
-        console.log("첫번째 좋아요! 포인트 적립!");
-        await commentRepo.savePointByComment(data);
+         // 아래 변수명에 포인트 적립여부가 담겨있음
+         const alreadySavePoint = await commentRepo.findSavedPointByComment(userId, commentId);
+         console.log('포인트받음?', alreadySavePoint);
+         // 첫번째 좋아요라 포인트적립이 되지 않았을 경우 적립
+         if (!alreadySavePoint) {
+            console.log('첫번째 좋아요! 포인트 적립!');
+            await commentRepo.savePointByComment(data);
+         }
+         return true; // 좋아요
+      } else {
+         console.log('좋아요 취소 끝.');
+         await commentRepo.unlikeCommentFromUser(userId, commentId);
+         return false; // 취소
       }
-      return true; // 좋아요
-    } else {
-      console.log("좋아요 취소 끝.");
-      await commentRepo.unlikeCommentFromUser(userId, commentId);
-      return false; // 취소
-    }
-  } catch (err) {
-    throw Error(err);
-  }
+   } catch (err) {
+      throw Error(err);
+   }
 };
 
 export const addComment = async (data: { userId: number; boardId: number; text: string }) => {
-  try {
-    const result = await commentRepo.addCommentQ(data);
-    console.log(result);
-    return result;
-  } catch (err) {
-    console.log(err);
-    throw new Error(`500, 서버오류`);
-  }
+   try {
+      const result = await commentRepo.addCommentQ(data);
+      console.log(result);
+      return result;
+   } catch (err) {
+      console.log(err);
+      throw new Error(`500, 서버오류`);
+   }
 };
 
 export const deleteComment = async (userId: number, boardId: number, commentId: number) => {
-  try {
-    const isComment = await commentRepo.findComment(commentId);
-    if (isComment === false) throw new Error(`404`);
+   try {
+      const isComment = await commentRepo.findComment(commentId);
+      if (isComment === false) throw new Error(`404`);
 
-    const result = await commentRepo.deleteCommentQ(userId, boardId, commentId);
-    return result;
-  } catch (err) {
-    if (err.message === `404`) throw new Error(`404, 댓글을 찾을 수 없습니다.`);
-    console.log(err.message);
-    throw new Error(`500, 서버 오류`);
-  }
+      const result = await commentRepo.deleteCommentQ(userId, boardId, commentId);
+      return result;
+   } catch (err) {
+      if (err.message === `404`) throw new Error(`404, 댓글을 찾을 수 없습니다.`);
+      console.log(err.message);
+      throw new Error(`500, 서버 오류`);
+   }
 };
 
 export const updateComment = async (userId: number, commentId: number, data: { text: string }) => {
-  // 해당 댓글이 토큰주인의 것인지 판단
-  try {
-    const user = await userRepo.findOneUser(userId);
-    const update = await commentRepo.updateCommentQ(commentId, data);
-    return update;
-  } catch (err) {
-    throw new Error(`500, 서버 오류`);
-  }
+   // 해당 댓글이 토큰주인의 것인지 판단
+   try {
+      const user = await userRepo.findOneUser(userId);
+      const update = await commentRepo.updateCommentQ(commentId, data);
+      return update;
+   } catch (err) {
+      throw new Error(`500, 서버 오류`);
+   }
 };
 
 export const moreCommentsPagenation = async (
-  firstRequest: number,
-  boardId: number,
-  userId: number,
-  count: number,
-  mark: string
+   firstRequest: number,
+   boardId: number,
+   userId: number,
+   count: number,
+   mark: string,
 ) => {
-  try {
-    if (firstRequest === 1) {
-      const comments = await commentRepo.firstGetComments(boardId, userId, count);
+   try {
+      if (firstRequest === 1) {
+         console.log('diqkf');
+         const comments = await commentRepo.firstGetComments(boardId, userId, count);
+         console.log('diqkf');
+         return comments;
+      }
+      console.log('diqkf2');
+      const comments = await commentRepo.moreCommentsPagenationQ(boardId, userId, count, mark);
+      console.log('diqkf2');
       return comments;
-    }
-    const comments = await commentRepo.moreCommentsPagenationQ(boardId, userId, count, mark);
-    return comments;
-  } catch (err) {
-    console.log(err.message);
-    throw new Error(`500, 서버 오류`);
-  }
+   } catch (err) {
+      console.log(err.message);
+      throw new Error(`500, 서버 오류`);
+   }
 };
